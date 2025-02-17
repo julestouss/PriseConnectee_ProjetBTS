08a40b48500bb807eab3bdcf2e9efb49fb7ebba5
redouner <jules.toussaint1@gmail.com>
2025-02-17 13:49:05 +0100

    implémentation du wifi(mode ap + sta) et création d'un fichier .h pour structurer le code + création d'un fichier constant.h

---
+++ main/projetE6.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <esp_log.h>
+#include <esp_check.h>
+#include <nvs_flash.h> // to flash the memory (Non volatile sorage = NVS)
+#include <string.h>
+#include "constants.h"
+#include "wifi_ap_sta.h"
+
+
+
+void app_main(void) {
+    // initialise nvs 
+    esp_err_t ret = nvs_flash_init(); // is use to check if an error occure and then init the nvs flash
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+            ESP_ERROR_CHECK(nvs_flash_erase());
+            ret = nvs_flash_init();
+            // if the nvs is not blank erase what is written in it 
+            }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_ERROR_CHECK(network_conf());
+
+    //define the wifi mod, here because it will be called first
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
+
+    
+
+    // check if something is going wronge while connecting to the ap
+    esp_err_t wifi_status = FAILURE;
+
+    wifi_status = ap_conf_wifi();
+    if (SUCCESS != wifi_status){
+        ESP_LOGI("WIFI ", "Failed to connect to the sta, dying ....");
+        return;
+    }
+
+    sta_conf_wifi("Altice_F524", "tscp1065");
+
+
+}
